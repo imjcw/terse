9c02f20b99dcaf0213da88e4b8a043a1f70db2aa
marvin <imjcw@imjcw.com>
2015-12-27 18:05:20 +0800

    init

--- app/Biz/AuthBiz.php
+++ app/Biz/AuthBiz.php
@@ -10,7 +10,7 @@
     {
         public function checkAuth($username = '', $password = '')
         {
-            $encrypt_password = $this->encrypt_password();
+            $encrypt_password = $this->encrypt_password($username,$password);
 
             $auth_service = new AdminService();
             $admin = $auth_service->checkUserInfo($username, $encrypt_password);
